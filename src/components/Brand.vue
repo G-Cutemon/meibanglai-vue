7721e9f619ae2140643cf84c7769ffaef365b34b
Cutemon <269482756@qq.com>
2018-05-14 10:12:05 +0800

    初次提交，尝试进行基于vue的开发

---
+++ src/components/Brand.vue
@@ -0,0 +1,52 @@
+<template>
+    <div  id="brand-list" class="list">
+        <div class="sort">
+            <ul class="clearfix">
+                <li class="fl one">
+                    <img src="<%:=LOGO_URL%><%:=data[i].brand_logo%>" />
+                </li>
+
+                <li class="fl two">
+                    <p style="font-weight: bold;font-size: 1rem;">公司名：
+                        <!-- <%:=data[i].campany_name%> -->
+                    </p>
+                    <p>参展产品：
+                        <!-- <%:=data[i].product_name%> -->
+                    </p>
+                    <!--<p class="show_box1-context_product">产品介绍：
+                                <%:=data[i].product%>
+                            </p>-->
+                    <p>品牌官网：
+                        <!-- <%:=data[i].website%> -->
+                    </p>
+                </li>
+                <li class="fl three">
+                    <a href="#">联系方式</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    new Vue({
+        el: '#brand-list',
+        data: {
+
+        },
+        methods: {
+            getData: function(){
+                this.$http.get('//meibanglai.com/data/getInformationList.do', {
+                    params:{
+                        "category":category,
+                        "type":type,
+                        "num":num
+                    }
+                })
+            }
+        },
+    })
+</script>
+
+<style>
+</style>
