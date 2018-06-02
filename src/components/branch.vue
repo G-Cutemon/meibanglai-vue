780d818ea8e81ca7703f03e9836f9252958ea1af
Cutemon <269482756@qq.com>
2018-06-02 11:17:51 +0800

    调整文件结构

---
+++ src/components/branch.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+		<div class="select-area text-c m-t-5">
+			<div class="area">
+				<select id="s_province" name="s_province" class="h-45 w-w100"></select>
+				<select id="s_city" name="s_city" class="h-45 w-w100"></select>
+				<select id="s_county" name="s_county" class="h-45 w-w100"></select>
+				<input type="text" id="s_town" placeholder="乡/镇  如:玉溪镇" class="h-45 w-w100"/>
+				<button class="btn btn-primary h-45 w-10 w-w100" id="search">搜索</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/news.vue
@@ -0,0 +1,34 @@
+<template>
+    <table id="news-list" class="table">
+        <tbody>
+            <tr class="active" v-for="(item, index) in dataList" :key="index">
+                <td>
+                    <a href="#">{{ substr(item) }}</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                
+            }
+        },
+        props: ["dataList"],
+        methods: {
+            substr(item){
+                if(item.message){
+                    // console.log(item.message)
+                    let message = item.message
+                    return message.substring(0, message.length - 5)
+                }
+            }
+        },
+    }
+</script>
+
+<style>
+</style>
